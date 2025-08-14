64d2e3d16f413e92b7911b955b160ab2144b9825
armoore777 <143210363+armoore777@users.noreply.github.com>
2025-08-14 10:54:02 -0700

    Update fig4_efig5.R

--- Code/fig4_efig5.R
+++ Code/fig4_efig5.R
@@ -299,7 +299,7 @@ mort_prop_all_subgroup_barplot <- ggplot(mort_prop_all_subgroup, aes(x = subgrou
   geom_bar(stat = "identity") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 30, hjust = 1)) +
-  scale_fill_manual(values = c("balanced" = "#999999", "lymphoid dysregulation" = "#B0A4E3", "myeloid dysregulation" = "lightskyblue", "hyperinflammatory" = "#F47B00"))+
+  scale_fill_manual(values = c("balanced" = "#999999", "lymphoid dysregulation" = "#B0A4E3", "myeloid dysregulation" = "lightskyblue", "system-wide" = "#F47B00"))+
   scale_x_discrete(labels = function(x) stringr::str_to_title(x)) +
   theme(legend.position = "none")+
   theme(axis.text.x = element_blank())+
